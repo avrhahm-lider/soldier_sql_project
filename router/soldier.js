4c1cd917d80496bda3a2b2dbf8182d8c3d276974
avrhahm <am500148148@gmail.com>
2026-07-14 22:20:17 +0300

    add router file

---
+++ router/soldier.js
@@ -0,0 +1,73 @@
+import express from "express";
+import service from "../service/soldier.js";
+import { isId } from "../dal/soldiers.js";
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  try {
+    const result = await service.creatSoldier(req.body);
+    res.status(result.status).json(result.data);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json(e);
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const result = await service.getSoldier(req.query);
+    res.status(result.status).json(result.data);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json(e);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!idValidtion(id)) return res.status(400).json({ message: "invalid id" });
+    const result = await service.getByID(id);
+    res.status(result.status).json(result.data);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json(e);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!idValidtion(id)) return res.status(400).json({ message: "invalid id" });
+    const result = await service.deleteSoldier(id);
+    res.status(result.status).json(result.data);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json(e);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!idValidtion(id)) return res.status(400).json({ message: "invalid id" });
+    const result = await service.creatSoldier(id, req.body);
+    res.status(result.status).json(result.data);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json(e);
+  }
+});
+
+router.patch("/:id/status", async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!idValidtion(id)) return res.status(400).json({ message: "invalid id" });
+    const result = await service.creatSoldier(id, req.body);
+    res.status(result.status).json(result.data);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json(e);
+  }
+});

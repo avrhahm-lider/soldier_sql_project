52495fa3f4817be496a9c9f942902633b0b7dd1e
avrhahm <am500148148@gmail.com>
2026-07-14 22:20:55 +0300

    update dal file

--- dal/soldiers.js
+++ dal/soldiers.js
@@ -1,9 +1,14 @@
-import mysql2 from 'mysql2/promise'
+import mysql2 from "mysql2/promise";
 
-const pool = mysql2.createPool()
+const pool = mysql2.createPool({
+  host: "locakhost",
+  user: "root",
+  database: "my_db",
+  connectionLimit: 10,
+});
 
-async function creatTable() {
-    return await pool.execute(`
+export async function creatTable() {
+  return await pool.execute(`
 CREATE TABLE soldiers (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
@@ -13,39 +18,50 @@ CREATE TABLE soldiers (
     age INT NOT NULL,
     status VARCHAR(15) DEFAULT 'active',
     createAT TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        )`)
+        )`);
 }
 
 async function creatSoldier(soldier = {}) {
-    return await pool.execute(`
-        inaert into soldiers (name, role, rank, unit, age) values(?,?,?,?,?)`[soldier.name, soldier.role, soldier.rank, soldier.unit, soldier.age])
+  return await pool.execute(
+    `
+        inaert into soldiers (name, role, rank, unit, age) values(?,?,?,?,?)`[
+      (soldier.name, soldier.role, soldier.rank, soldier.unit, soldier.age)
+    ]
+  );
 }
-    const options = ["unit", "rank", "status"]
-    let getBy = ""
-    for (let i = 0; i < 3; i++){
-        if (qp[options[i]])
-            getBy += `${options[i]}=${qp[options[i]]}`
-    }
+
 async function getSoldier(getBy) {
-    return await pool.execute(`SELECT * FROM soldiers WHERE ${getBy}` )
-    
+  if (getBy.length === 0) return await pool.execute(`SELECT * FROM soldiers`);
+  return await pool.execute(`SELECT * FROM soldiers WHERE ${getBy}`);
 }
 
 async function getByID(id) {
-    return await pool.execute("SELECT * FROM soldiers WHERE id=?",[id])
+  return await pool.execute("SELECT * FROM soldiers WHERE id=?", [id]);
 }
 
-async function updateSoldier(id, update ={}) {
-    const newData = [update.name, update.role, update.rank, update.unit, update.status, id]
-    return await pool.execute('UPDATE soldier SET name=?, role=?, rank=?, unit=?, status=? WHERE id=?',newData)
-    
+async function updateSoldier(id, update = {}) {
+  const newData = [update.name, update.role, update.rank, update.unit, update.status, id];
+  return await pool.execute(
+    "UPDATE soldier SET name=?, role=?, rank=?, unit=?, status=? WHERE id=?",
+    newData
+  );
 }
 
 async function deleteByID(id) {
-    return await pool.execute("DELELTE  FROM soldiers WHERE id=?",[id])
+  return await pool.execute("DELELTE  FROM soldiers WHERE id=?", [id]);
 }
 
-async function updateSoldier(id, status) {
-    const newData = [update.name, update.role, update.rank, update.unit, update.status, id]
-    return await pool.execute('UPDATE soldier SET status=?',[status])
+async function updateStatus(id, status) {
+  return await pool.execute("UPDATE soldier SET status=? WHERE id=?", [status, id]);
 }
+export async function isId(id) {
+  return await pool.execute("select count(*) from soldiers WHERE id=?", [id]);
+}
+export default {
+  creatSoldier,
+  getSoldier,
+  getByID,
+  deleteByID,
+  updateStatus,
+  updateSoldier,
+};
